a003d03f48fb8a5bae76e3228cd4ff02b514640b
David Mendonça <davidmed66@gmail.com>
2020-12-17 20:12:21 -0300

    Exerc1

--- exerc1.js
+++ exerc1.js
@@ -1,2 +1,23 @@
-const string = 'kkkrrrrivffkkllll';
-const int = 2;
+const sequencia = 'aaaabbbbcdf'; //aabbcdf
+const limite = 3;
+const removeRepetido = (sequencia, limite) => {
+    let contador = 0;
+    let sequenciaNova = "";
+    for (let i = 0; i < sequencia.length; i++) {
+        if (sequencia[i] == sequencia[i + 1]) {
+            contador++;
+            if (contador < limite) {
+                console.log("entrou no incremento de string")
+                sequenciaNova += sequencia[i];
+            }
+        } else {
+            console.log(contador + " entrou no else")
+            contador = 0;
+            sequenciaNova += sequencia[i];
+        }
+        console.log(contador)
+    }
+    return sequenciaNova
+}
+
+console.log(removeRepetido(sequencia, limite));
