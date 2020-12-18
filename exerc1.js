965be341c7604a08efd5b50e13540e2ed16c1925
David Mendonça <davidmed66@gmail.com>
2020-12-18 12:08:49 -0300

    Exclusão de alguns console.log e finalizaçã/correção do exerc2

--- exerc1.js
+++ exerc1.js
@@ -1,5 +1,3 @@
-const sequencia = 'aaaabbbbcdf'; //aabbcdf
-const limite = 3;
 const removeRepetido = (sequencia, limite) => {
     let contador = 0;
     let sequenciaNova = "";
@@ -7,17 +5,14 @@ const removeRepetido = (sequencia, limite) => {
         if (sequencia[i] == sequencia[i + 1]) {
             contador++;
             if (contador < limite) {
-                console.log("entrou no incremento de string")
                 sequenciaNova += sequencia[i];
             }
         } else {
-            console.log(contador + " entrou no else")
             contador = 0;
             sequenciaNova += sequencia[i];
         }
-        console.log(contador)
     }
     return sequenciaNova
 }
 
-console.log(removeRepetido(sequencia, limite));
+console.log(removeRepetido("xxxyzzaaayy", 2));
