bc7211c55cfd01a687ae80bc6c1cf8f376a48f51
David Mendonça <davidmed66@gmail.com>
2020-12-19 09:18:13 -0300

    Exclusão de espaços

--- exerc2.js
+++ exerc2.js
@@ -1,27 +1,20 @@
 const removeChar = (sequencia, multiplicador) => {
     let sequenciaNova = "";
-
     for (let i = 0; i < sequencia.length; i++) {
-
         if (sequencia[i] !== "?") {
             let recebe = "";
             sequenciaNova += recebe.concat(sequencia[i])
-
         } else {
-
             if (sequencia[i - 1] == undefined) {
                 let temp = sequencia;
                 let recebeTemp = temp.split("?")
                 let junta = recebeTemp.join('');
                 let charMulti = "";
-
                 for (let k = 0; k < multiplicador; k++) {
                     charMulti += junta[junta.length - 1]
                 }
                 sequenciaNova += charMulti
-
             } else {
-
                 let charMulti = "";
                 for (let k = 0; k < multiplicador; k++) {
                     charMulti += sequencia[i - 1]
